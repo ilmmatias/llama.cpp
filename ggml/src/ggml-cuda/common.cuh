7f8461369cd3f801a8a5fd0efd65ef12e291334e
ilmmatias <93882972+ilmmatias@users.noreply.github.com>
2026-08-24 13:59:10 -0300

    cuda: recapture graphs after memory pool flushes
    Assisted by: GPT-5.6 Sol

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -1246,6 +1246,7 @@ struct ggml_cuda_graph {
     bool warmup_complete = false;
     uint64_t uid = 0;
     int64_t last_used_time = 0;
+    uint64_t pool_flush_count = UINT64_MAX;
     struct node_properties {
         ggml_tensor node;
         void *   node_src_data_ptrs[GGML_MAX_SRC];
@@ -1673,4 +1674,3 @@ static __inline__ void ggml_cuda_kernel_launch(Kernel kernel, const ggml_cuda_ke
     kernel<<<launch_params.block_nums, launch_params.block_dims, launch_params.shmem, launch_params.stream>>>(std::forward<Args>(args)... );
     CUDA_CHECK(cudaGetLastError());
 }
-
